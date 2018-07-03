f6ee950a5cc9aad7105e055f158beda02ab11ad4
okba bekhelifi <okba.bekhelifi@gmail.com>
2018-07-03 17:40:45 +0100

    Added find matlab path function
     Changes to be committed:
    	new file:   findMatlabPath.m

---
+++ findMatlabPath.m
@@ -0,0 +1,9 @@
+function [matlabPath] = findMatlabPath()
+%FINDMATLABPATH Summary of this function goes here
+%   Detailed explanation goes here
+% created 07-03-2018
+% last modification -- -- --
+% Okba Bekhelifi, <okba.bekhelif@univ-usto.dz>
+matlabPath = strcat( matlabroot, '\bin\matlab.exe');
+end
+
